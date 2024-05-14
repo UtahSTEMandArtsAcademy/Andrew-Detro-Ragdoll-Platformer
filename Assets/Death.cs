8f0d3f5efb489948d35afa9cbbd3f91f8713c045
UtahSTEMandArtsAcademy <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-05-14 17:01:03 -0600

    Level design, particles on death

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+    public GameObject PS;
     private void OnTriggerEnter(Collider other)
     {
+        GameObject clone;
+        clone = Instantiate(PS, other.gameObject.transform.position, other.gameObject.transform.rotation);
         Destroy(other.gameObject);
+        Destroy(clone, 3f);
     }
 }
